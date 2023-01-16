134eb2bd0203cf485ba58610d53984fa02f12612
irvingmx <iramirezg@gmail.com>
2023-01-16 14:09:52 +0100

    Initial commit

---
+++ src/main/java/irvingmx/microservices/multiplication/MultiplicationApplication.java
@@ -0,0 +1,13 @@
+package irvingmx.microservices.multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiplicationApplication.class, args);
+    }
+
+}
